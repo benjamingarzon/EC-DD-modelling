2ff223d5110f91922ff2ea192b081625949077ed
Benjamin Garzon Jimenez de Cisneros <benjamin.garzon@ife.uzh.ch>
2024-04-30 07:43:47 +0000

    Parameter recovery analyses

--- stats/run_ddm_fits.sh
+++ stats/run_ddm_fits.sh
@@ -11,6 +11,9 @@ cd ../jags
 NSAMPLES=10000 #
 NBURNIN=50000 #
 
+Rscript FitDDMtoTaskData.R processed_data_censored-simulated.RData linear_drift_no_pooling_ddm $NSAMPLES $NBURNIN > ../logs/log-linear_drift_no_pooling_ddm-${NSAMPLES}-${NBURNIN}.txt &
+
+exit 1
 #Rscript FitDDMtoTaskData.R processed_data_censored.RData linear_drift_no_pooling_corr_ddm $NSAMPLES $NBURNIN > ../logs/log-linear_drift_no_pooling_corr_ddm-${NSAMPLES}-${NBURNIN}.txt &
 Rscript FitDDMtoTaskData.R processed_data_censored.RData linear_drift_no_pooling_bd_ddm $NSAMPLES $NBURNIN > ../logs/log-linear_drift_no_pooling_ddm-${NSAMPLES}-${NBURNIN}.txt &
 Rscript FitDDMtoTaskData.R processed_data_censored.RData linear_drift_no_pooling_ddm $NSAMPLES $NBURNIN > ../logs/log-linear_drift_no_pooling_ddm-${NSAMPLES}-${NBURNIN}.txt &
